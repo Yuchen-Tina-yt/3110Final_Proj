0e69d2c3934a36a798396fbd80f0dd4c19b5ab9e
Yuchen Tian <yt523@cornell.edu>
2019-11-05 21:37:42 -0500

    commiting player files, player ml and player nli

---
+++ player.mli
@@ -0,0 +1,19 @@
+
+(** The abstract type of values representing the player *)
+type t 
+
+
+(**[change_wealth player amt] is the player after changing the player's wealth.*)
+val change_wealth: t -> int -> t
+
+(**[add_item player item] is the player after adding an item to the player's 
+   items list *)
+val add_item: t -> Places.item -> t
+
+(**[remove_item player item] is the player after deleting an item 
+   from the player;s items list *)
+val remove_item: t -> Places.item -> t
+
+(**[move_pos player steps places] is the player after moving number of steps 
+   of steps from the player's current position on the map *)
+val move_pos: t -> int -> Places.places_lst -> t
