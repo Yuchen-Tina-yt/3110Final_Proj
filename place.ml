0c7f860c2ac0e0f366996a25b7e37d55449e6855
as3484 <as3484@cornell.edu>
2019-11-06 13:33:36 -0500

    removing module stuff

--- place.ml
+++ place.ml
@@ -1,20 +1,16 @@
-module Place = struct 
+type t = {name:string; country: string; value: float; owner: Player option; 
+          num_houses: int; rent_payment: int option; pass_payment: int option}
+let make_place name country value owner num_houses rent_payment pass_payment =
+  {name = name; counter = country; value = value; owner = owner; 
+   num_houses = num_houses; rent_payment = rent_payment;
+   pass_payment=pass_payment}
 
-  type t = {name:string; country: string; value: float; owner: Player option; 
-            num_houses: int; rent_payment: int option; pass_payment: int option}
-  let make_place name country value owner num_houses rent_payment pass_payment =
-    {name = name; counter = country; value = value; owner = owner; 
-     num_houses = num_houses; rent_payment = rent_payment;
-     pass_payment=pass_payment}
+let change_ownership t player =
+  {name= t.name; country =t.country; value=t.value; owner=player; 
+   num_houses=t.num_houses; rent_payment=
+                              t.rent_payment; pass_payment=t.pass_payment}
 
-  let change_ownership t player =
-    {name= t.name; country =t.country; value=t.value; owner=player; 
-     num_houses=t.num_houses; rent_payment=
-                                t.rent_payment; pass_payment=t.pass_payment}
-
-  let change_land_value t new_val = 
-    {name= t.name; country=t.country; value = new_val; owner =t.owner; 
-     num_houses=t.num_houses; rent_payment = 
-                                t.rent_payment; pass_payment=t.pass_payment}
-
-end 
+let change_land_value t new_val = 
+  {name= t.name; country=t.country; value = new_val; owner =t.owner; 
+   num_houses=t.num_houses; rent_payment = 
+                              t.rent_payment; pass_payment=t.pass_payment}
